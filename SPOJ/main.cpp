1ddbcf41136fccc2fad0e6b09cc6064220dc382e
Taguhi Abgaryan <taguhiabgaryan@taguhis-macbook-pro.local>
2017-05-20 14:23:13 +0400

    Initial Commit

---
+++ SPOJ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SPOJ
+//
+//  Created by Taguhi Abgaryan on 5/20/17.
+//  Copyright © 2017 Taguhi Abgaryan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
